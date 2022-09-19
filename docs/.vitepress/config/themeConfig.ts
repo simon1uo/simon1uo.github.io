3b974a935986742267cda045dbd2dcafda79966f
Simon Luo <simonlaw910@gmail.com>
2022-09-19 12:27:39 +0800

    feat: add edit link

--- docs/.vitepress/config/themeConfig.ts
+++ docs/.vitepress/config/themeConfig.ts
@@ -4,15 +4,21 @@ import nav from "./nav";
 const themeConfig: DefaultTheme.Config = {
   logo: "/logo.svg",
   nav,
-  footer: {
-    copyright: "Copyright © 2022 Simon Luo",
-  },
   socialLinks: [
     {
       icon: "github",
       link: "http://github.com/simon1uo/simon1uo.github.io",
     },
   ],
+
+  editLink: {
+    text: "Edit",
+    pattern:
+      "https://github.com/simon1uo/simon1uo.github.io/edit/main/docs/:path",
+  },
+  footer: {
+    copyright: "Copyright © 2022 Simon Luo",
+  },
 };
 
 export default themeConfig;
