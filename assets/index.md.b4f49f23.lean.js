06cf4cb281ca24178a4cd126da2b07886fdd304f
simon1uo <simonlaw910@gmail.com>
2022-10-10 17:10:21 +0800

    Deploying to gh-pages from @ simon1uo/simon1uo.github.io@6d8382cd6a7b8134c76bda3f303cf119bea25aec 🚀

---
+++ assets/index.md.b4f49f23.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./app.b0f7a99b.js";const _=JSON.parse('{"title":"main","description":"","frontmatter":{"layout":"home","title":"main","sidebar":false},"headers":[],"relativePath":"index.md"}'),n={name:"index.md"};function o(r,s,i,c,d,m){return t(),a("div")}const l=e(n,[["render",o]]);export{_ as __pageData,l as default};
